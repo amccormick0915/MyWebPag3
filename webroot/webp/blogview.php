4dc52e3a47d6387b000dc9ad1e17711253c98636
Mc Cormick A <annelynmccormick@yahoo.com>
2020-05-04 01:54:56 +0100

    changing sort of blog

--- webroot/webp/blogview.php
+++ webroot/webp/blogview.php
@@ -68,7 +68,8 @@ while($data_row = mysqli_fetch_array($result)){
     // echo "<br> other testing: " .  " " . $rowss[0][0] . "<br>";
     //                              this one outputs array and this "blog details" 
 
-
+ echo "<br>one more test: " . $rows[0]["created"] ;
+ echo "<br>one more test: " . $rows[1]["created"] ;
 //  echo "<br>test: " . $rowss[0][0]." test " . $rows[0][1] ." " . $rows[0][2]." " . $rows[0][3] ." " . $rows[0][4] ." " . $rows[0]["blog_details"]." " ;
 
 while( $counter < sizeof($row) ){
